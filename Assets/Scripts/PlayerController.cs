c1df1749a16e5f29dbae249804b40915a2a9d1e9
LAPTOP-N8E0I7QK\gotou <gotoukei57@gmail.com>
2023-04-08 23:58:37 +0900

    ジャンプ、横移動時のロジックを皮だけ作成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,24 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Playerの操作一覧
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rigid2D;
-    private float jumpForce = 680.0f;
+    private float jumpForce;
+    private float runForce;
 
     void Start()
     {
         // アタッチを行ったオブジェトのRigidコンポーネントにアクセス
         this.rigid2D = GetComponent<Rigidbody2D>();
+        this.jumpForce = 600.0f;
+        this.walkForce = 30.0f;
     }
 
     void Update()
     {
+        // ジャンプボタンクリック時の処理
+        caseJumpButton();
+
+        // 左右ボタンクリック時の処理
+        caseMoveSideButton();
+    }
+
+    void caseJumpButton()
+    {
         // スペースボタンが押されたか
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(isJumpButton())
         {
-            jump();
+            float haight = calculateJumpHaight();
+            jump(haight);
         }
+    }
+
+    float calculateJumpHaight()
+    {
+        return this.jumpForce * 0.01;
+    }
+
+    void caseMoveSideButton()
+    {
+        if(isRightButton())
+        {
+            float speedx = calculateMoveSpeedX();
+            moveRight(speedx);
+        }
+
+        if(isLeftButton())
+        {
+            float speedx = calculateMoveSpeedX();
+            moveLeft(speedx);
+        }
+    }
+
+    float calculateSpeedX()
+    {
+        return Mathf.Abs(this.rigid2D.velocity.x);
+    }
+
+
+    void isJumpButton()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+
+    void moveLeft(float speed)
+    {
+        this.rigid2D.AddForce(transform.left * speed);
+    }
+
+    void moveRight()
+    {
+        this.rigid2D.AddForce(transform.right * speed);
+    }
+
+    bool isRight()
+    {
+        return false;
+    }
+
+    bool isLeft()
+    {
+        return false;
     }
 
     void jump()
